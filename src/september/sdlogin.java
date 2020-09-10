48a4788f4cbbb8641b79a991468e83c848c7d8a5
footballPowai <footballpowai@gmail.com>
2020-09-11 00:55:37 +0530

    vishal added sd login / create lead / assign agent casses

---
+++ src/september/sdlogin.java
@@ -0,0 +1,28 @@
+package september;
+
+public class sdlogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("sd login");
+
+	}
+
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("sd create lead");
+
+	}
+
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("assign agent");
+
+	}
+
+	
+
+}
