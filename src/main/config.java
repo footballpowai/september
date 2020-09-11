e1e8b35ff88388f8a2768dbd25935ba5369abae7
footballPowai <footballpowai@gmail.com>
2020-09-12 04:01:47 +0530

    added cofig by vishal

--- src/main/config.java
+++ src/main/config.java
@@ -4,5 +4,7 @@ public class config {
 	String url = "staging";
 	String url1 = "prod";
 	String url2 = "local3000";
-
+	String url3 = "local3000";
+	String url4 = "local3000";
+	String url5 = "local3000";
 }
