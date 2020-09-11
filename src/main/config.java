5c5167bfa5d88804c1993c0598563fd72495e3d1
footballPowai <footballpowai@gmail.com>
2020-09-12 04:09:31 +0530

    added few more config by sanjay

--- src/main/config.java
+++ src/main/config.java
@@ -7,4 +7,9 @@ public class config {
 	String url3 = "local3000";
 	String url4 = "local3000";
 	String url5 = "local3000";
+	String url6 = "local3000";
+	String url7 = "local3000";
+	String url8 = "local3000";
+	String url9 = "local3000";
+	String url10 = "local3000";
 }
